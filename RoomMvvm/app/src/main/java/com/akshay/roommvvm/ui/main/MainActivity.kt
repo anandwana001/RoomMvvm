07ce73727de9eaabc511485cdaa663c98a15a901
anandwana001 <akshaynandwana001@gmail.com>
2020-03-19 18:04:13 +0530

    init activity result api

--- RoomMvvm/app/src/main/java/com/akshay/roommvvm/ui/main/MainActivity.kt
+++ RoomMvvm/app/src/main/java/com/akshay/roommvvm/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.akshay.roommvvm.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akshay.roommvvm.R
@@ -24,6 +25,17 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     @Inject
     lateinit var userAdapter: UserAdapter
+
+    val launcher = prepareCall(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            it.data?.let { extras ->
+                val reply = extras.getStringExtra(AddActivity.EXTRA_REPLY)
+                reply?.let { name ->
+                    viewModel.addDataToDatabase(name)
+                }
+            }
+        }
+    }
 
     override fun provideLayoutId(): Int = R.layout.activity_main
 
@@ -51,28 +63,15 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
         viewModel.launchAddData.observe(this, Observer {
             it.getIfNotHandled()?.run {
-                startActivityForResult(
-                    Intent(applicationContext, AddActivity::class.java),
-                    REQUEST_CODE
-                )
+                launcher.launch(Intent(this@MainActivity, AddActivity::class.java))
             }
         })
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
-            REQUEST_CODE -> {
-                if (resultCode == RESULT_OK) {
-                    data?.let {
-                        val reply = it.getStringExtra(AddActivity.EXTRA_REPLY)
-                        reply?.let {
-                            viewModel.addDataToDatabase(it)
-                        }
-                    }
-                }
-            }
-
             1001 -> {
                 if (resultCode == RESULT_OK) {
                     data?.let {
